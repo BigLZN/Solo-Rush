bd5ff031114968fd465c4eca96dfabc890ea7977
BigLZN <lznpro51@gmail.com>
2024-03-21 22:20:16 +0100

    modif site complet

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
-let slideIndex = 0;
-let intervalId;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlide(n) {
-  const slides = document.querySelectorAll(".slide");
-  const dots = document.querySelectorAll(".dot");
-
-  if (n >= slides.length) {
-    slideIndex = 0;
-  } else if (n < 0) {
-    slideIndex = slides.length - 1;
-  }
-
-  slides.forEach((slide) => {
-    slide.style.display = "none";
-  });
-
-  dots.forEach((dot) => {
-    dot.classList.remove("active");
-  });
-
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].classList.add("active");
-}
-
-function nextSlide() {
-  slideIndex++;
-  showSlide(slideIndex);
-}
-
-function prevSlide() {
-  slideIndex--;
-  showSlide(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  slideIndex = n;
-  showSlide(slideIndex);
+  showSlides((slideIndex = n));
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  showSlide(slideIndex);
-  intervalId = setInterval(nextSlide, 5000);
-});
-
-document.querySelectorAll(".slide, .dot").forEach((element) => {
-  element.addEventListener("click", function () {
-    clearInterval(intervalId);
-  });
-});
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
